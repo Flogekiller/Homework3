d12275db2946a7095d401f900863d16d00f588b0
Flogekiller <florian.schnoetzlinger@gmail.com>
2022-09-30 13:11:47 +0200

    WeaponsOhne1.6

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,13 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class FileHandler
 {
     public List<Weapons> weaponsList = new ArrayList<>();
-    public void csvLesen()
+   public void csvLesen()
     {
         String[] splittet;
         File file = new File("C:\\Users\\flori\\Documents\\Schule\\3.Klasse\\POS\\Hausübung3\\Homework3\\src\\Weapons.csv");
@@ -21,9 +25,21 @@ public class FileHandler
                 Weapons weapons = new Weapons(splittet[0], CombatType.valueOf(splittet[1]), DmgType.valueOf(splittet[2]), Integer.parseInt(splittet[3]), Integer.parseInt(splittet[4]), Integer.parseInt(splittet[5]), Integer.parseInt(splittet[6]));
                 weaponsList.add(weapons);
             }
-            weaponsList.forEach(System.out::println);
+
 
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void csvLesenMitStream()
+    {
+        String path = "C:\\Users\\flori\\Documents\\Schule\\3.Klasse\\POS\\Hausübung3\\Homework3\\src\\Weapons.csv";
+        try {
+
+            Files.lines(Path.of(path)).forEach(System.out::println);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -8,6 +9,17 @@ public class Main
         Main main = new Main();
         FileHandler fileHandler = new FileHandler();
         fileHandler.csvLesen();
+        Printable printable = (w -> w.forEach(System.out::println));
+        main.sortCombatType(fileHandler.weaponsList);
+        printable.print(fileHandler.weaponsList);
+        System.out.println("\n");
+        main.sortDmg(fileHandler.weaponsList);
+        printable.print(fileHandler.weaponsList);
+        System.out.println("\n");
+        main.sortName(fileHandler.weaponsList);
+        printable.print(fileHandler.weaponsList);
+
+
     }
 
     private List<Weapons> sortDmg(List<Weapons> weaponsList)
@@ -18,6 +30,11 @@ public class Main
     private List<Weapons> sortCombatType(List<Weapons> weaponsList)
     {
         Collections.sort(weaponsList, Comparator.comparing(p -> p.getCombatType()));
+        return weaponsList;
+    }
+    private List<Weapons> sortName(List<Weapons> weaponsList)
+    {
+        Collections.sort(weaponsList, Comparator.comparing(p -> p.getName()));
         return weaponsList;
     }
 
